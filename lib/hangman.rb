f65bf0ff3889f6cac7d1b04640a0b61fb4147058
joaohrn <joaohrn0@gmail.com>
2023-03-06 21:27:42 -0300

    Implements saving and loading

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 module AvailableWords
   WORDS = File.readlines('google-10000-english-no-swears.txt')
 end
@@ -5,7 +6,7 @@ end
 class Computer
   include AvailableWords
   def choose_word()
-    AvailableWords::WORDS[rand(10000)].chomp
+    AvailableWords::WORDS[rand(10_000)].chomp
   end
 end
 
@@ -14,13 +15,12 @@ class Human
     loop do
       puts 'Type your guess:'
       choice = gets.chomp
-      if choice.length == 1
+      if(('a'..'z').include?(choice) || choice == '1')
         return choice
       else
         puts 'invalid input try again'
-      end  
+      end
     end
-
   end
 end
 
@@ -30,34 +30,78 @@ class Game
     @computer_player = Computer.new
   end
 
+  def new_game
+    @chosen_word = @computer_player.choose_word.split ''
+    @hidden_word = @chosen_word.map { '_' }
+    @wrong_guesses = []
+    @number_of_attempts = 8
+  end
+
+  def load_game
+    save = File.open('saves/save.yml', 'r')
+    loaded_game = YAML.load save
+    save.close
+    @chosen_word = loaded_game[:chosen_word]
+    @hidden_word = loaded_game[:hidden_word]
+    @wrong_guesses = loaded_game[:wrong_guesses]
+    @number_of_attempts = loaded_game[:number_of_attempts]
+  end
+
+  def save_game
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+    File.open('saves/save.yml', 'w') do |file|
+      save_data = {
+        chosen_word: @chosen_word,
+        hidden_word: @hidden_word,
+        wrong_guesses: @wrong_guesses,
+        number_of_attempts: @number_of_attempts
+      }
+      file.puts YAML.dump(save_data)
+      break
+    end
+  end
+
+  def start_game
+    puts 'Type 1 to start a new game'
+    puts 'Type 2 to load a save'
+    choice = gets.chomp
+    if choice == '1'
+      new_game
+      play_game
+    elsif choice == '2' && File.exist?('saves/save.yml')
+      load_game
+      play_game
+    end
+  end
+
   def play_game()
-    chosen_word = @computer_player.choose_word.split ''
-    hidden_word = chosen_word.map { '_' }
-    wrong_guesses = []
-    number_of_attempts = 8
-    while number_of_attempts > 0
-      puts "#{hidden_word}"
-      puts "You have #{number_of_attempts} attempts left."
-      puts "Wrong guesses: #{wrong_guesses}"
+    while @number_of_attempts.positive?
+      puts @hidden_word.to_s
+      puts "You have #{@number_of_attempts} attempts left."
+      puts "Wrong guesses: #{@wrong_guesses}"
+      puts 'type 1 to save and quit'
       choice = @human_player.get_input.downcase
-      if chosen_word.include?(choice)
-        chosen_word.each_with_index do |char, ind|
-          hidden_word[ind] = choice if char == choice 
+      if choice == '1'
+        save_game
+        return nil
+      elsif @chosen_word.include?(choice)
+        @chosen_word.each_with_index do |char, ind|
+          @hidden_word[ind] = choice if char == choice 
         end
-      elsif wrong_guesses.include?(choice) || hidden_word.include?(choice)
+      elsif @wrong_guesses.include?(choice) || @hidden_word.include?(choice)
         puts 'Already guessed this one.'
       else
-        wrong_guesses << choice
-        number_of_attempts -= 1
+        @wrong_guesses << choice
+        @number_of_attempts -= 1
       end
-      if hidden_word.join('') == chosen_word.join('')
-        puts "You win it was #{chosen_word.join ''}"
+      if @hidden_word.join('') == @chosen_word.join('')
+        puts "You win it was #{@chosen_word.join ''}"
         return
       end
     end
-    puts "you lose, it was #{chosen_word.join ''}"
+    puts "you lose, it was #{@chosen_word.join ''}"
   end
 end
 
 game = Game.new
-game.play_game
+game.start_game
